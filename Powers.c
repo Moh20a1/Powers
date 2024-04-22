07acc7da69cf7bbb8d8ea7335b4aef95037bd3f8
Moh20 o <121547395+Moh20a1@users.noreply.github.com>
2024-04-22 21:46:09 +0300

    Fixing Powers.cfixing Powers.c

--- Powers.c
+++ Powers.c
@@ -1,11 +1,12 @@
-e#include <math.h>
+#include <stdio.h>
+#include <math.h>
 
 int main()
 {
 	float value,result;
 	int power;
 	
-	printf("enter a float and then a power: (hint, press space to seprate them) ");
+	printf("enter a number and then a power: (hint, press space to seprate them) ");
 	scanf("%f %d", &value,&power);
 	result = pow(value,power);
 	printf("%.2f to the power of %d = %.2f", value,power,result);
