7363cc00f8c9dd015a323603ed4b0eb4eac08ff7
Moh20 o <121547395+Moh20a1@users.noreply.github.com>
2024-04-22 21:14:55 +0300

    adding intital files

---
+++ Powers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float value,result;
+	int power;
+	
+	printf("enter a float and then a power: (hint, press space to seprate them) ");
+	scanf("%f %d", &value,&power);
+	result = pow(value,power);
+	printf("%.2f to the power of %d = %.2f", value,power,result);
+	return(0);
+}
